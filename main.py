184884c15e60a152b78b5c6a3853840300c6b527
Dimitar Tanev <jim.tanev@gmail.com>
2018-01-28 13:49:50 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import cv2
-import numpy as np
 import skeletonization as skel
 
 
 def main():
 
-    img_skeleton = skel.execute('fingerprints/pic2.tif')
+    img_skeleton = skel.execute('fingerprints/101_1.tif')
 
     # find harris corners
-    img_skeleton = np.float32(img_skeleton)
     dst = cv2.cornerHarris(img_skeleton, 2, 3, 0.04)
-    dst = cv2.dilate(dst, None)
     ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
-    dst = np.uint8(dst)
 
     cv2.imshow("skel", img_skeleton)
     cv2.imshow("dst", dst)
